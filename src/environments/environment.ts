69c77ef3a5e20f71b26459df3a3e85da1a741fb6
Edowardo <edowardotan@gmail.com>
2023-09-25 17:27:37 +0800

    initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    baseURL: 'https://c701-223-255-229-67.ngrok-free.app',
+    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImVkb3dhcmRvdGFuQGdtYWlsLmNvbSIsImlhdCI6MTY5NTYwODIxNywiZXhwIjoxNzAyODA4MjE3fQ.8jlsROeKGWoI0WbD6Hxweg3x_9Quzsb9MP-LoDF2RHY",
+};
